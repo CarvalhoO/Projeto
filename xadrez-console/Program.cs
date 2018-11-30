f8edcc587f0ee8d4aaa52233f64aa2d2b40eb19c
rmartins53 <45485259+rmartins53@users.noreply.github.com>
2018-11-30 11:16:24 +0000

    Add files via upload

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using tabuleiro;
+using xadrez;
+
+namespace xadrez_console {
+    class Program {
+        static void Main(string[] args) {
+
+            try {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+
+                while (!partida.terminada) {
+
+                    try {
+                        Console.Clear();
+                        Ecra.imprimirPartida(partida);
+
+                        Console.WriteLine();
+                        Console.Write("Origem: ");
+                        Posicao origem = Ecra.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
+
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                        Console.Clear();
+                        Ecra.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Ecra.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
+
+                        partida.realizaJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e) {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
+                }
+                Console.Clear();
+                Ecra.imprimirPartida(partida);
+            }
+            catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
